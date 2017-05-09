f05f3f9a2fcb8eb57a5d46132ef8ef036300f4f9
Arne Brasseur <arne@arnebrasseur.net>
2017-05-09 09:17:34 +0200

    Heroku setup and linting

--- app.js
+++ app.js
@@ -26,14 +26,13 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/redirectims.html', (req, res) => {
-  console.log("==> redirectims.html")
+  console.log('==> redirectims.html')
   console.log(JSON.stringify(req.url))
   console.log(JSON.stringify(req.query))
   console.log(JSON.stringify(req.headers))
-  res.header("Content-type", "text/html")
+  res.header('Content-type', 'text/html')
   res.send(fs.readFileSync('public/redirectims_.html').toString().replace(/CLIENT_ID/, config.apiKey))
 })
-
 
 app.get('/', (_, res) => {
   res.render('index', {title: 'hello', clientId: config.apiKey})
@@ -44,12 +43,12 @@ app.get('/webhook', (req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-  console.log("===> POST /")
+  console.log('===> POST /')
   console.log(JSON.stringify(req.headers))
-  console.log("------------------------------------------------------------")
+  console.log('------------------------------------------------------------')
   console.log(JSON.stringify(req.body))
-  console.log("============================================================")
-  res.send("ok")
+  console.log('============================================================')
+  res.send('ok')
 })
 
 module.exports = app
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,38 +1,29 @@
-var csdkLoginBtn = document.getElementById("csdk-login");
-csdkLoginBtn.addEventListener('click', handleCsdkLogin, false);
+var csdkLoginBtn = document.getElementById('csdk-login')
+csdkLoginBtn.addEventListener('click', handleCsdkLogin, false)
 
-var access_token = "";
+var access_token = ''
 
 AdobeCreativeSDK.init({
-    clientID: CONFIG.CSDK_CLIENT_ID,
-    onError: function(error) {
-        if (error.type === AdobeCreativeSDK.ErrorTypes.AUTHENTICATION) {
-            /*
-            	Note: this error will occur when you try
-                to launch a component without checking if
-            	the user has authorized your app.
-
-            	From here, you can trigger
-                AdobeCreativeSDK.loginWithRedirect().
-            */
-            console.log('You must be logged in to use the Creative SDK');
-        } else if (error.type === AdobeCreativeSDK.ErrorTypes.GLOBAL_CONFIGURATION) {
-            console.log('Please check your configuration');
-        } else if (error.type === AdobeCreativeSDK.ErrorTypes.SERVER_ERROR) {
-            console.log('Oops, something went wrong');
-        }
+  clientID: CONFIG.CSDK_CLIENT_ID,
+  onError: function (error) {
+    if (error.type === AdobeCreativeSDK.ErrorTypes.AUTHENTICATION) {
+      console.log('You must be logged in to use the Creative SDK')
+    } else if (error.type === AdobeCreativeSDK.ErrorTypes.GLOBAL_CONFIGURATION) {
+      console.log('Please check your configuration')
+    } else if (error.type === AdobeCreativeSDK.ErrorTypes.SERVER_ERROR) {
+      console.log('Oops, something went wrong')
     }
-});
+  }
+})
 
-
-function handleCsdkLogin() {
-  AdobeCreativeSDK.getAuthStatus(function(csdkAuth) {
-        if (csdkAuth.isAuthorized) {
-            access_token = csdkAuth.accessToken;
-            csdkLoginBtn.disabled = true;
-            csdkLoginBtn.value = "Authorized";
-        } else {
-            AdobeCreativeSDK.login(handleCsdkLogin);
-        }
-    });
+function handleCsdkLogin () {
+  AdobeCreativeSDK.getAuthStatus(function (csdkAuth) {
+    if (csdkAuth.isAuthorized) {
+      access_token = csdkAuth.accessToken
+      csdkLoginBtn.disabled = true
+      csdkLoginBtn.value = 'Authorized'
+    } else {
+      AdobeCreativeSDK.login(handleCsdkLogin)
+    }
+  })
 }
